b3e3d59141098657baf10b855ce2b7ce0b8f1a76
Gennadii <Ligr98@gmail.com>
2021-04-12 23:54:55 +0300

    HW_2

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, register_converter
+from app.converters import DtConverter
+from app.views import file_list, file_content
 
 # Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
+
+register_converter(DtConverter, 'format')
 
 
 urlpatterns = [
     # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
-                                      # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    path('', file_list, name='file_list'),  # для детальной информации смотрите HTML-шаблоны в директории templates
+    path('<format:date>/', file_list, name='file_list'),    # задайте необязательный параметр "date"
+    path('files/<str:name>/', file_content, name='file_content'),
 ]
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,44 @@
 import datetime
-
+import os
+from django.http import HttpResponse
 from django.shortcuts import render
+from app import settings
+
+FILES_ARRAY = os.listdir(settings.FILES_PATH)
 
 
-def file_list(request):
+def file_list(request, date=None):
     template_name = 'index.html'
-    
+    all_files = []
+    context = {}
+    if date:
+        context['date'] = date.date()
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
-    }
-
-    return render(request, template_name, context)
+    for file in FILES_ARRAY:
+        file_stat = os.stat(f'{settings.FILES_PATH}/{file}')
+        file_info = {
+            'name': file,
+            'ctime': datetime.datetime.fromtimestamp(file_stat.st_ctime),
+            'mtime': datetime.datetime.fromtimestamp(file_stat.st_mtime),
+        }
+        all_files.append(file_info)
+    context['files'] = all_files
+    return render(request, template_name, context=context)
 
 
 def file_content(request, name):
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
-
+    file_path = os.path.join(settings.FILES_PATH, name)
+    if os.path.exists(file_path):
+        with open(os.path.join(settings.FILES_PATH, name),
+                  encoding='utf-8') as f:
+            file_data = f.read()
+        return render(
+            request,
+            'file_content.html',
+            context={
+                'file_name': name,
+                'file_content': file_data
+            }
+        )
+    else:
+        return HttpResponse("404. Запрошенный файл не существует", status=404)
